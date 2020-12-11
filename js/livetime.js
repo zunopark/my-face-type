34f6e369bbeadd80f6af1400dc0522f91ef6f225
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-12-11 12:26:30 +0900

    livetime

--- js/livetime.js
+++ js/livetime.js
@@ -7,13 +7,13 @@ if (countNum) {
   function handleCountNum() {
     let countNumber = 7209999;
     function countUp() {
-      if (countNumber < 11362627) {
+      if (countNumber < 11452627) {
         countNumber = countNumber + 37994;
         countNum.innerHTML = countNumber;
       }
     }
     setInterval(countUp, 1);
-    time.innerHTML = `(20.12.10 10:00 기준)`;
+    time.innerHTML = `(20.12.11 12:00 기준)`;
   }
   handleCountNum();
 }
