1783ed9ad0ba73df661a2ca3955b7da5fdb8272d
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-12-22 01:18:14 +0900

    feat: crazy up

--- js/livetime.js
+++ js/livetime.js
@@ -7,7 +7,7 @@ if (countNum) {
   function handleCountNum() {
     let countNumber = 7209999;
     function countUp() {
-      if (countNumber < 11862627) {
+      if (countNumber < 12362627) {
         countNumber = countNumber + 37994;
         countNum.innerHTML = countNumber;
       }
