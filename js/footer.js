5ca687d2c473142eb9196fdc1de98c9032e93046
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2021-01-01 20:37:32 +0900

    feat

--- js/footer.js
+++ js/footer.js
@@ -2,8 +2,8 @@ const footer = document.querySelector(`.footer`);
 
 function init() {
   footer.innerHTML = ` <div class="contact">
-    <a href="mailto:zzunopark@gmail.com" class="contact_me">개발자 문의</a>
-    <a href="mailto:ghdtlr90@naver.com" class="contact_me">광고 문의</a>
+    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">개발자 문의</a>
+    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">광고 문의</a>
   </div>
   <div class="footer__icon">
     <i class="fas fa-portrait"></i>
