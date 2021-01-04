c56ea682a1460abd333bba390f394baf0f9d9e30
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2021-01-04 17:58:46 +0900

    feat

--- js/livetime.js
+++ js/livetime.js
@@ -1,4 +1,23 @@
 // 실시간 js
+
+const countNum = document.querySelector(`.count__num`);
+
+if (countNum) {
+  getTime();
+  function handleCountNum() {
+    let countNumber = 2209999;
+    function countUp() {
+      if (countNumber < 17432627) {
+        countNumber = countNumber + 57994;
+        countNum.innerHTML = countNumber;
+      }
+    }
+    setInterval(countUp, 1);
+  }
+
+  handleCountNum();
+}
+
 function getTime() {
   let currentTime = new Date();
   let year = currentTime.getFullYear();
@@ -7,9 +26,8 @@ function getTime() {
   let hour = currentTime.getHours();
 
   const time = document.querySelector(`.timer`);
+
   time.innerHTML = `(${year}.${month < 10 ? `0${month}` : month}.${
     date < 10 ? `0${date}` : date
   } ${hour < 10 ? `0${hour}` : hour}:00 기준)`;
 }
-
-getTime();
