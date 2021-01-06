6d09c07a30ac483c01277531fd344ccfbe8fba37
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2021-01-06 13:55:20 +0900

    house

---
+++ house/js/ad.js
@@ -0,0 +1,87 @@
+// 상단 광고 js
+const adUpper = document.querySelector(`.ad__upper`);
+const adUpperBtn = document.querySelector(`.ad__upper__hide__btn`);
+
+function handleAdUpper() {
+  if (adUpper.classList[2] === undefined) {
+    adUpper.classList.add("snackbar__anim__up");
+  } else if (adUpper.classList[2] === "snackbar__anim__up") {
+    adUpper.classList.remove("snackbar__anim__up");
+  }
+}
+
+if (adUpper) {
+  adUpperBtn.addEventListener("click", handleAdUpper);
+  setTimeout(function () {
+    adUpper.classList.add("snackbar__anim__result");
+  }, 1000);
+}
+
+//배너 광고 js
+const superImg = document.querySelector(`.super__img`);
+if (superImg) {
+  function handleSuper() {
+    var newWindow = window.open("about:blank");
+    newWindow.location.href =
+      "https://ceo.supermembers.co.kr/?utm_source=naver&utm_medium=cpc&utm_campaign=2020faceApp&utm_term=faceApp&utm_content=faceAppAd";
+  }
+  superImg.addEventListener("click", handleSuper);
+}
+
+// const dosaImg = document.querySelector(`.dosa__img`);
+// if (dosaImg) {
+//   function handleDosa() {
+//     var newWindow = window.open("about:blank");
+//     newWindow.location.href = "https://bit.ly/3ogfHm7";
+//   }
+//   dosaImg.addEventListener("click", handleDosa);
+// }
+
+const linerImg = document.querySelector(`.liner__img`);
+if (linerImg) {
+  function handleLiner() {
+    var newWindow = window.open("about:blank");
+    newWindow.location.href =
+      "https://getliner.com/?utm_source=site_listing&utm_medium=banner&utm_campaign=physiognomy_kr&utm_content=mock_up_free_web_pdf_hi";
+  }
+  linerImg.addEventListener("click", handleLiner);
+}
+
+const helloImg = document.querySelector(`.hello__img`);
+if (helloImg) {
+  function handleHello() {
+    var newWindow = window.open("about:blank");
+    newWindow.location.href = "testmake.html";
+  }
+  helloImg.addEventListener("click", handleHello);
+}
+
+// const todayWeather = document.querySelector(`.today__weather__wrap`);
+// const blackBoard = document.querySelector(`.black__board`);
+// const closeBtn = document.querySelector(`.upper__comment`);
+// const shareBtn = document.querySelector(`.lower__button`);
+
+// function handleCloseBtn() {
+//   todayWeather.classList.add("disblock");
+//   blackBoard.classList.add("disblock");
+// }
+
+// if (closeBtn) {
+//   closeBtn.addEventListener("click", handleCloseBtn);
+//   blackBoard.addEventListener("click", handleCloseBtn);
+// }
+// shareBtn.addEventListener("click", handleCopy);
+
+// setTimeout(function () {
+//   todayWeather.classList.remove("disblock");
+//   blackBoard.classList.remove("disblock");
+// }, 10);
+
+// function handleCopy() {
+//   var urlLink = document.getElementById("shareUrl");
+//   console.log(window.document.location.href, urlLink);
+//   urlLink.value = window.document.location.href;
+//   urlLink.select();
+//   document.execCommand("copy");
+//   urlLink.blur();
+// }
---
+++ house/js/livetime.js
@@ -0,0 +1,33 @@
+// 실시간 js
+
+const countNum = document.querySelector(`.count__num`);
+
+if (countNum) {
+  getTime();
+  function handleCountNum() {
+    let countNumber = 2209999;
+    function countUp() {
+      if (countNumber < 19332627) {
+        countNumber = countNumber + 57994;
+        countNum.innerHTML = countNumber;
+      }
+    }
+    setInterval(countUp, 1);
+  }
+
+  handleCountNum();
+}
+
+function getTime() {
+  let currentTime = new Date();
+  let year = currentTime.getFullYear();
+  let month = currentTime.getMonth() + 1;
+  let date = currentTime.getDate();
+  let hour = currentTime.getHours();
+
+  const time = document.querySelector(`.timer`);
+
+  time.innerHTML = `(${year}.${month < 10 ? `0${month}` : month}.${
+    date < 10 ? `0${date}` : date
+  } ${hour < 10 ? `0${hour}` : hour}:00 기준)`;
+}
