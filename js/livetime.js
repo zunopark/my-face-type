34e22f1f7426baff39d896eb34a69f00f4b74653
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2021-01-07 13:41:25 +0900

    original

--- js/livetime.js
+++ js/livetime.js
@@ -7,7 +7,7 @@ if (countNum) {
   function handleCountNum() {
     let countNumber = 2209999;
     function countUp() {
-      if (countNumber < 19332627) {
+      if (countNumber < 20732627) {
         countNumber = countNumber + 57994;
         countNum.innerHTML = countNumber;
       }
