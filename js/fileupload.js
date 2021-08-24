7aeba4975695cfab1f9b4bb78ab3520aa1cc2449
dydtjq94 <dydtjq94@yonsei.ac.kr>
2021-08-24 15:25:46 +0900

    test

--- js/fileupload.js
+++ js/fileupload.js
@@ -358,6 +358,53 @@ async function predict() {
   </a>`
   labelContainer.appendChild(supermembers)
 
+  const habitownBannerWrap = document.querySelector(`.habitown__banner__wrap`)
+  const habitownBanner = document.querySelector(`.habitown__banner`)
+  const downloadBtn = document.querySelector(`.app__download__btn`)
+  const downloadBottonBtn = document.querySelector(
+    `.habitown__download__btn__bottom`,
+  )
+
+  const cancelNeverBtn = document.querySelector(`.cancel__btn__left`)
+
+  const cancelBtn = document.querySelector(`.cancel__btn__right`)
+  const blackBoard = document.querySelector(`.black__board`)
+
+  const never = localStorage.getItem('never')
+
+  if (!never) {
+    setTimeout(function () {
+      habitownBannerWrap.classList.add('appear')
+    }, 1500)
+  }
+
+  function handleGoAppDownload() {
+    location.href = 'http://onelink.to/habitown'
+    habitownBanner.classList.add('disblock')
+    blackBoard.classList.add('disblock')
+  }
+
+  function handleCancelBanner() {
+    habitownBanner.classList.add('disblock')
+    blackBoard.classList.add('disblock')
+  }
+
+  function handleCancelNeverBanner() {
+    localStorage.setItem('never', 'ok')
+    habitownBanner.classList.add('disblock')
+    blackBoard.classList.add('disblock')
+  }
+
+  function habitownInit() {
+    downloadBtn.addEventListener('click', handleGoAppDownload)
+    downloadBottonBtn.addEventListener('click', handleGoAppDownload)
+    cancelBtn.addEventListener('click', handleCancelBanner)
+    blackBoard.addEventListener('click', handleCancelBanner)
+    cancelNeverBtn.addEventListener('click', handleCancelNeverBanner)
+  }
+
+  habitownInit()
+
   // const subscribeBigWrap = document.querySelector(`.subscribe__big__wrap`);
   // subscribeBigWrap.classList.remove("none");
 
--- js/habitown.js
+++ js/habitown.js
@@ -1,47 +1,46 @@
-const habitownBannerWrap = document.querySelector(`.habitown__banner__wrap`)
+// const habitownBannerWrap = document.querySelector(`.habitown__banner__wrap`)
+// const habitownBanner = document.querySelector(`.habitown__banner`)
+// const downloadBtn = document.querySelector(`.app__download__btn`)
+// const downloadBottonBtn = document.querySelector(
+//   `.habitown__download__btn__bottom`,
+// )
 
-const habitownBanner = document.querySelector(`.habitown__banner`)
-const downloadBtn = document.querySelector(`.app__download__btn`)
-const downloadBottonBtn = document.querySelector(
-  `.habitown__download__btn__bottom`,
-)
+// const cancelNeverBtn = document.querySelector(`.cancel__btn__left`)
 
-const cancelNeverBtn = document.querySelector(`.cancel__btn__left`)
+// const cancelBtn = document.querySelector(`.cancel__btn__right`)
+// const blackBoard = document.querySelector(`.black__board`)
 
-const cancelBtn = document.querySelector(`.cancel__btn__right`)
-const blackBoard = document.querySelector(`.black__board`)
+// const never = localStorage.getItem('never')
 
-const never = localStorage.getItem('never')
+// if (!never) {
+//   setTimeout(function () {
+//     habitownBannerWrap.classList.add('appear')
+//   }, 1500)
+// }
 
-if (!never) {
-  setTimeout(function () {
-    habitownBannerWrap.classList.add('appear')
-  }, 1500)
-}
+// function handleGoAppDownload() {
+//   location.href = 'http://onelink.to/habitown'
+//   habitownBanner.classList.add('disblock')
+//   blackBoard.classList.add('disblock')
+// }
 
-function handleGoAppDownload() {
-  location.href = 'http://onelink.to/habitown'
-  habitownBanner.classList.add('disblock')
-  blackBoard.classList.add('disblock')
-}
+// function handleCancelBanner() {
+//   habitownBanner.classList.add('disblock')
+//   blackBoard.classList.add('disblock')
+// }
 
-function handleCancelBanner() {
-  habitownBanner.classList.add('disblock')
-  blackBoard.classList.add('disblock')
-}
+// function handleCancelNeverBanner() {
+//   localStorage.setItem('never', 'ok')
+//   habitownBanner.classList.add('disblock')
+//   blackBoard.classList.add('disblock')
+// }
 
-function handleCancelNeverBanner() {
-  localStorage.setItem('never', 'ok')
-  habitownBanner.classList.add('disblock')
-  blackBoard.classList.add('disblock')
-}
+// function habitownInit() {
+//   downloadBtn.addEventListener('click', handleGoAppDownload)
+//   downloadBottonBtn.addEventListener('click', handleGoAppDownload)
+//   cancelBtn.addEventListener('click', handleCancelBanner)
+//   blackBoard.addEventListener('click', handleCancelBanner)
+//   cancelNeverBtn.addEventListener('click', handleCancelNeverBanner)
+// }
 
-function habitownInit() {
-  downloadBtn.addEventListener('click', handleGoAppDownload)
-  downloadBottonBtn.addEventListener('click', handleGoAppDownload)
-  cancelBtn.addEventListener('click', handleCancelBanner)
-  blackBoard.addEventListener('click', handleCancelBanner)
-  cancelNeverBtn.addEventListener('click', handleCancelNeverBanner)
-}
-
-habitownInit()
+// habitownInit()
