2d79cc87103dc764f03f6de35df96a7dadf1e627
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-02-20 15:08:04 +0900

    refiled

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ const bottomBtn = document.querySelector(`.bottom_ad_btn`)
 
 function init() {
   footer.innerHTML = ` <div class="contact">
-    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">테스트제작 문의</a>
+    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">개발자 문의</a>
     <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">광고 문의</a>
   </div>
   <div class="footer__icon">
--- js/livetime.js
+++ js/livetime.js
@@ -31,3 +31,33 @@ function getTime() {
     date < 10 ? `0${date}` : date
   } ${hour < 10 ? `0${hour}` : hour}:00 기준)`
 }
+
+const randomBanner = document.querySelector(`.randomBanner`)
+const randomNum = Math.floor(Math.random() * 2) + 1
+
+if (randomNum == 1) {
+  randomBanner.innerHTML = `
+  <a
+    class="dang__img"
+    href=" https://refilled.co.kr/shop?utm_source=keen?utm_campaign=keen?utm_medium=banner?utm_content=scanner"
+    onclick="gtag('event', '리필드스캐너이미지', {'event_category':
+  '리필드스캐너이미지','event_label': '리필드스캐너이미지'});"
+  ><img
+  src="https://i.ibb.co/L9pfTYB/Kakao-Talk-Photo-2023-02-20-15-03-28-001.jpg"
+  alt="scanner"
+/>
+  </a>`
+} else {
+  randomBanner.innerHTML = `
+  <a
+    class="dang__img"
+    href="https://refilled.co.kr/shop?utm_source=keen?utm_campaign=keen?utm_medium=banner?utm_content=booster"
+    onclick="gtag('event', '리필드부스터이미지', {'event_category':
+  '리필드부스터이미지','event_label': '리필드부스터이미지'});"
+  >
+  <img
+  src="https://i.ibb.co/2Fhd1N3/booster.jpg"
+  alt="scanner"
+/>
+  </a>`
+}
