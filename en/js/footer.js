c823ed6fd91d7671e2f8e1e1196fb72e26856ddb
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-02-28 13:45:10 +0900

    lang en

---
+++ en/js/footer.js
@@ -0,0 +1,13 @@
+const footer = document.querySelector(`.footer`)
+
+function init() {
+  footer.innerHTML = ` <div class="contact">
+    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">Contact developer</a>
+    <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">Ads inquiries</a>
+  </div>
+  <div class="footer__icon">
+    <i class="fas fa-portrait"></i>
+  </div>
+  <span class="footer__text"> AI.Face 2023 &copy;</span>`
+}
+init()
