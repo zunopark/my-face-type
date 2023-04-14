53a6a99b3cb899127ad88907a0991d507bcd48c8
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-04-14 12:31:24 +0900

    gtag

--- destiny/js/main.js
+++ destiny/js/main.js
@@ -2,7 +2,10 @@ const yangbanAppBtn = document.querySelector(`.yangban_app_btn`)
 
 const handleAppBtn = () => {
   location.href = 'http://pf.kakao.com/_Qfuvxj/chat'
-  console.log('click')
+  gtag('event', '카카오 플러스 친구 사주', {
+    event_category: '카카오 플러스 친구 사주',
+    event_label: '카카오 플러스 친구 사주',
+  }
 }
 
 yangbanAppBtn.addEventListener('click', handleAppBtn)
--- tarot/js/main.js
+++ tarot/js/main.js
@@ -2,7 +2,10 @@ const yangbanAppBtn = document.querySelector(`.yangban_app_btn`)
 
 const handleAppBtn = () => {
   location.href = 'http://pf.kakao.com/_Qfuvxj/chat'
-  console.log('click')
+  gtag('event', '카카오 플러스 친구 타로', {
+    event_category: '카카오 플러스 친구 타로',
+    event_label: '카카오 플러스 친구 타로',
+  }
 }
 
 yangbanAppBtn.addEventListener('click', handleAppBtn)
