ce8141c319c70d5a1b2b1bd82053adba59094489
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-04-28 17:54:51 +0900

    app version add

--- js/footer.js
+++ js/footer.js
@@ -2,12 +2,14 @@ const footer = document.querySelector(`.footer`)
 const bottomAd = document.querySelector(`.bottom_ad`)
 const headerForAd = document.querySelector(`.header`)
 
+const appVersion = 'v0.79'
+
 function init() {
   footer.innerHTML = ` <div class="contact">
   <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">광고 문의</a>
   <a href="mailto:dydtjq94@yonsei.ac.kr" class="contact_me">개발 문의</a>
   </div>
-  <span class="footer__text"> AI양반 2023 &copy;</span>`
+  <span class="footer__text"> AI양반 2023 &copy;</span> ${appVersion}`
 
   // if (bottomAd) {
   //   if (headerForAd.id === 'face') {
