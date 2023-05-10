e50a525f0f88dd3563bcae84d94f9d1c11b7c675
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-05-11 00:40:32 +0900

    fortune

--- dream/js/main.js
+++ dream/js/main.js
@@ -4,11 +4,10 @@ const loaderWrap = document.querySelectorAll(`.loader`)
 let userMessages = []
 let assistantMessages = []
 let myDateTime = ''
-let userInfo = {}
 
 const spinnerHTML = () => {
   loaderWrap.forEach(function (content) {
-    content.innerHTML = `<div class="loader_wrap"><i class="fa fa-spinner fa-spin"></i><div class="spinner_comment">운세를 분석중입니다.</div></div>`
+    content.innerHTML = `<div class="loader_wrap"><i class="fa fa-spinner fa-spin"></i><div class="spinner_comment">해몽가가 꿈을 풀이하는 중입니다.</div></div>`
   })
 }
 
@@ -24,56 +23,27 @@ const start = async () => {
   const resultTotal = document.querySelector('.fortune_result_total')
   const bottomAdFile = document.querySelector(`.bottom_ad`)
 
-  const name = document.querySelector(`.fortune_name`).value
-  const relationship = document.querySelector(`.fortune_relationship`).value
-  const gender = document.querySelector(`.fortune_gender`).value
-  const dateType = document.querySelector(`.fortune_birth_type`).value
-  const date = document.querySelector('.fortune_date').value
-  const time = document.querySelector('.fortune_time').value
+  const dreamContent = document.querySelector(`.dream_main`).value
 
-  if (name === '') {
-    alert('이름을 입력해주세요.')
+  if (dreamContent === '') {
+    alert('꿈 내용을 입력해주세요.')
     return
   }
 
   myDateTime = date + time
 
-  userInfo = {
-    name,
-    relationship,
-    gender,
-    dateType,
-    date,
-    time,
-  }
-
-  document.querySelector(`.fortune_user_name`).innerHTML = userInfo.name
-  document.querySelector(`.fortune_user_relationship`).innerHTML =
-    userInfo.relationship
-  document.querySelector(`.fortune_user_gender`).innerHTML = userInfo.gender
-  document.querySelector(
-    `.fortune_user_birth_type`,
-  ).innerHTML = `(${userInfo.dateType})`
-  document.querySelector('.fortune_user_date').innerHTML = userInfo.date
-  document.querySelector('.fortune_user_time').innerHTML = `${
-    userInfo.time === '' ? '태어난 시간 모름' : userInfo.time
-  }`
-
-  localStorage.setItem('UserInfo', JSON.stringify(userInfo))
-
   spinnerAppear('loader')
-  document.querySelector(`.fortune_start_btn_wrap`).style.display = 'none'
+  document.querySelector(`.dream_start_btn_wrap`).style.display = 'none'
 
-  userMessages.push('내가 어제 백호 꿈을 꿨는데 꿈 풀이를 해줘!')
+  userMessages.push(`내가 꾼 꿈은 ${dreamContent} 이런 내용인데 꿈 풀이좀 해줘`)
 
-  // const fortuneResultGtag = () => {
-  //   gtag('event', '오늘의 운세 User', {
-  //     event_category: '오늘의 운세',
-  //     event_label: `오늘의 운세 User 이름: ${name}, 관계: ${relationship}, 성별: ${gender}, 생일타입: ${dateType}, 생일: ${date}, 태어난시간: ${time}`,
-  //   })
-  // }
-
-  // fortuneResultGtag()
+  const dreamResultGtag = () => {
+    gtag('event', '해몽꿈 Content', {
+      event_category: '해몽꿈 Content',
+      event_label: `해몽 꿈 작성 내용: ${dreamContent}`,
+    })
+  }
+  dreamResultGtag()
 
   const response = await fetch(
     'https://fkfucds3e9.execute-api.ap-northeast-2.amazonaws.com/prod/dreamTell',
@@ -83,8 +53,6 @@ const start = async () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        userInfo: userInfo,
-        myDateTime: myDateTime,
         userMessages: userMessages,
         assistantMessages: assistantMessages,
       }),
@@ -94,7 +62,7 @@ const start = async () => {
   const data = await response.json()
 
   document.getElementById('intro').style.display = 'none'
-  document.querySelector('.fortune_user_info_wrap').style.display = 'block'
+
   document.querySelector('.fortune_result_wrap').style.display = 'block'
 
   spinnerDisappear('loader')
@@ -109,57 +77,37 @@ const start = async () => {
   resultTotal.appendChild(astrologerMessage)
 }
 
-const result = async (type) => {
-  const resultContentWrap = document.querySelector(
-    `.fortune_result_` + `${type}`,
-  )
-  let userMessageContent = ''
+// const result = async (type) => {
+//   userMessages.push(userMessageContent)
 
-  spinnerAppear(`loader_` + `${type}`)
-  document.querySelector('.result_' + `${type}` + '_btn').style.display = 'none'
+//   const response = await fetch(
+//     'https://fkfucds3e9.execute-api.ap-northeast-2.amazonaws.com/prod/dreamTell',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         myDateTime: myDateTime,
+//         userInfo: userInfo,
+//         userMessages: userMessages,
+//         assistantMessages: assistantMessages,
+//       }),
+//     },
+//   )
 
-  if (type === 'love') {
-    userMessageContent = '오늘 나의 애정운을 알려줘'
-  } else if (type === 'wealth') {
-    userMessageContent = '오늘 나의 재물운을 알려줘'
-  } else if (type === 'health') {
-    userMessageContent = '오늘 나의 건강운을 알려줘'
-  } else if (type === 'business') {
-    userMessageContent = '오늘 나의 사업운을 알려줘'
-  } else if (type === 'study') {
-    userMessageContent = '오늘 나의 학업운을 알려줘'
-  }
+//   const data = await response.json()
 
-  userMessages.push(userMessageContent)
+//   spinnerDisappear(`loader_` + `${type}`)
 
-  const response = await fetch(
-    'https://fkfucds3e9.execute-api.ap-northeast-2.amazonaws.com/prod/dreamTell',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        myDateTime: myDateTime,
-        userInfo: userInfo,
-        userMessages: userMessages,
-        assistantMessages: assistantMessages,
-      }),
-    },
-  )
+//   //assistantMessage 메세지 추가
+//   assistantMessages.push(data.assistant)
 
-  const data = await response.json()
-
-  spinnerDisappear(`loader_` + `${type}`)
-
-  //assistantMessage 메세지 추가
-  assistantMessages.push(data.assistant)
-
-  const astrologerMessage = document.createElement('div')
-  astrologerMessage.classList.add('fortune_result_content')
-  astrologerMessage.innerHTML = `${data.assistant}`
-  resultContentWrap.appendChild(astrologerMessage)
-}
+//   const astrologerMessage = document.createElement('div')
+//   astrologerMessage.classList.add('fortune_result_content')
+//   astrologerMessage.innerHTML = `${data.assistant}`
+//   resultContentWrap.appendChild(astrologerMessage)
+// }
 
 const sendMessage = async () => {
   const chatInput = document.querySelector('.chat-input input')
@@ -204,26 +152,9 @@ const sendMessage = async () => {
   chatBox.appendChild(astrologerMessage)
 }
 
-const fortuneUserData = () => {
-  const userData = localStorage.getItem('UserInfo')
-  let user = JSON.parse(userData)
-
-  if (user != null) {
-    document.querySelector(`.fortune_name`).value = user.name
-    document.querySelector(`.fortune_relationship`).value = user.relationship
-    document.querySelector(`.fortune_gender`).value = user.gender
-    document.querySelector(`.fortune_birth_type`).value = user.dateType
-    document.querySelector('.fortune_date').value = user.date
-    document.querySelector('.fortune_time').value = user.time
-  }
-}
-
 const fortuneUserChangeBtn = document.querySelector(`.fortune_user_change`)
 
-fortuneUserChangeBtn.addEventListener('click', handleFortuneReset)
-
 function handleFortuneReset() {
-  localStorage.removeItem('UserInfo')
   location.reload(true)
   location.href = location.href
   history.go(0)
@@ -231,11 +162,6 @@ function handleFortuneReset() {
 
 const initFortuneMain = () => {
   spinnerHTML()
-  fortuneUserData()
-
-  // document
-  //   .querySelector('.chat-input button')
-  //   .addEventListener('click', sendMessage)
 }
 
 initFortuneMain()
--- js/header.js
+++ js/header.js
@@ -2,132 +2,34 @@ const headerContent = () => {
   const headerMain = document.querySelector(`.header`)
 
   if (headerMain.id === 'face') {
-    headerMain.innerHTML = ` <div class="header__upper">
+    headerMain.innerHTML = ` <div class="header__upper header_fixed">
   <div class="header__title">
-    <div class="header__title__title">AI양반</div>
+    <a href="/" class="header__title__title">관상가 양반</a>
     <div class="header_title_date"></div>
   </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content header_nav_select">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
-
 </div>`
   } else if (headerMain.id === 'fortune') {
-    headerMain.innerHTML = ` <div class="header__upper">
+    headerMain.innerHTML = ` <div class="header__upper header_fixed">
   <div class="header__title">
-    <div class="header__title__title">AI양반</div>
+    <a href="/fortune" class="header__title__title">역술가 양반</a>
     <div class="header_title_date"></div>
   </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content header_nav_select">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
 </div>`
-  } else if (headerMain.id === 'destiny') {
-    headerMain.innerHTML = ` <div class="header__upper">
+  } else if (headerMain.id === 'dream') {
+    headerMain.innerHTML = ` <div class="header__upper header_fixed">
   <div class="header__title">
-    <div class="header__title__title">AI양반</div>
+    <a href="/dream" class="header__title__title">해몽가 양반</a>
     <div class="header_title_date"></div>
   </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content header_nav_select">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
-</div>`
-  } else if (headerMain.id === 'compatibility') {
-    headerMain.innerHTML = ` <div class="header__upper">
-  <div class="header__title">
-    <div class="header__title__title">AI양반</div>
-    <div class="header_title_date"></div>
-  </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content header_nav_select">궁합</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
-</div>`
-  } else if (headerMain.id === 'constellation') {
-    headerMain.innerHTML = ` <div class="header__upper">
-  <div class="header__title">
-    <div class="header__title__title">AI양반</div>
-    <div class="header_title_date"></div>
-  </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/constellation" class="header_nav_content header_nav_select">별자리</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
-</div>`
-  } else if (headerMain.id === 'tarot') {
-    headerMain.innerHTML = ` <div class="header__upper">
-  <div class="header__title">
-    <div class="header__title__title">AI양반</div>
-    <div class="header_title_date"></div>
-  </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/tarot" class="header_nav_content header_nav_select">타로</a>
-  <a href="/lotto" class="header_nav_content">로또</a>
-</div>`
-  } else if (headerMain.id === 'lotto') {
-    headerMain.innerHTML = ` <div class="header__upper">
-  <div class="header__title">
-    <div class="header__title__title">AI양반</div>
-    <div class="header_title_date"></div>
-  </div>
-</div>
-<div class="header_nav_wrap">
-  <a href="/" class="header_nav_content">
-    관상
-  </a>
-  <a href="/fortune" class="header_nav_content">운세</a>
-  <a href="/destiny" class="header_nav_content">사주</a>
-  <a href="/compatibility" class="header_nav_content">궁합</a>
-  <a href="/tarot" class="header_nav_content">타로</a>
-  <a href="/lotto" class="header_nav_content header_nav_select">로또</a>
 </div>`
   }
+
+  const mainContentWrap = this.document.querySelector(`.main_content_wrap`)
+  mainContentWrap.classList.add('main_content_wrap_top_margin')
 }
 
 const headerGetTime = () => {
   const headerDate = document.querySelector(`.header_title_date`)
-
   let today = new Date()
   let year = today.getFullYear()
   let month = today.getMonth() + 1
@@ -163,38 +65,40 @@ const headerGetTime = () => {
   }.${date < 10 ? `0${date}` : date} ${dayOfWeekString}`
 }
 
-const headerFixedFunc = () => {
-  window.addEventListener('scroll', function () {
-    const headerNavWrap = document.querySelector('.header_nav_wrap')
-    const mainContentWrap = this.document.querySelector(`.main_content_wrap`)
-    if (window.pageYOffset > 68) {
-      headerNavWrap.classList.add('header_fixed')
-      if (mainContentWrap) {
-        mainContentWrap.classList.add('main_content_wrap_top_margin')
-      }
-    } else {
-      headerNavWrap.classList.remove('header_fixed')
-      if (mainContentWrap) {
-        mainContentWrap.classList.remove('main_content_wrap_top_margin')
-      }
-    }
-  })
-}
+// const headerFixedFunc = () => {
+//   window.addEventListener('scroll', function () {
+//     const headerNavWrap = document.querySelector('.header_nav_wrap')
+//     const mainContentWrap = this.document.querySelector(`.main_content_wrap`)
+//     mainContentWrap.classList.add('main_content_wrap_top_margin')
 
-const goHeaderHome = () => {
-  const headerTitle = document.querySelector(`.header__title__title`)
+//     if (window.pageYOffset > 68) {
+//       headerNavWrap.classList.add('header_fixed')
+//       if (mainContentWrap) {
+//         mainContentWrap.classList.add('main_content_wrap_top_margin')
+//       }
+//     } else {
+//       headerNavWrap.classList.remove('header_fixed')
+//       if (mainContentWrap) {
+//         mainContentWrap.classList.remove('main_content_wrap_top_margin')
+//       }
+//     }
+//   })
+// }
 
-  headerTitle.addEventListener('click', function (event) {
-    event.preventDefault()
-    window.location.href = '/'
-  })
-}
+// const goHeaderHome = () => {
+//   const headerTitle = document.querySelector(`.header__title__title`)
+
+//   headerTitle.addEventListener('click', function (event) {
+//     event.preventDefault()
+//     window.location.href = '/'
+//   })
+// }
 
 function init() {
   headerContent()
   headerGetTime()
-  headerFixedFunc()
-  goHeaderHome()
+  // headerFixedFunc()
+  // goHeaderHome()
 }
 
 init()
