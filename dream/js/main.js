d88d1148ee3377b821641b73641abfc6935e7c3d
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-05-11 21:26:45 +0900

    test

--- dream/js/main.js
+++ dream/js/main.js
@@ -40,7 +40,7 @@ const getDreamTell = async (text) => {
 
 const getDreamSummary = async (text) => {
   const response2 = await fetch(
-    'https://fkfucds3e9.execute-api.ap-northeast-2.amazonaws.com/prod/dreamTellSummary',
+    'https://fkfucds3e9.execute-api.ap-northeast-2.amazonaws.com/prod/dreamSummary',
     {
       method: 'POST',
       headers: {
@@ -83,9 +83,9 @@ const start = async () => {
   dreamResultGtag()
 
   const totalDreamTeller = await getDreamTell(userMessages)
-  // const dataSummaryMessages = `${totalDreamTeller.assistant} 라는 꿈을 꿨는데 30자 이하의 완성된 문장으로 요약해줘`
+  const dataSummaryMessages = `${totalDreamTeller.assistant} 라는 꿈을 꿨는데 30자 이하의 완성된 문장으로 요약해줘`
   // const dataSummaryMessages = `이빨 빠지는 꿈을 꿨는데 30자 이하의 완성된 문장으로 요약해줘`
-  // const summaryDreamTeller = await getDreamSummary(dataSummaryMessages)
+  const summaryDreamTeller = await getDreamSummary(dataSummaryMessages)
   console.log(totalDreamTeller)
 
   document.querySelector('.fortune_result_wrap').style.display = 'block'
@@ -93,12 +93,13 @@ const start = async () => {
   spinnerDisappear('loader')
 
   //assistantMessage 메세지 추가
-  assistantMessages.push(data.assistant)
+  // assistantMessages.push(data.assistant)
 
+  // 결과 생성
   const astrologerMessage = document.createElement('div')
   astrologerMessage.classList.add('fortune_result_content')
-  astrologerMessage.innerHTML = `${dreamContent} // ${totalDreamTeller.assistant}`
-  // astrologerMessage.innerHTML = `${dreamContent} //  //${summaryDreamTeller.assistant}`
+  // astrologerMessage.innerHTML = `${dreamContent} // ${totalDreamTeller.assistant}`
+  astrologerMessage.innerHTML = `${dreamContent} // ${totalDreamTeller.assistant} //${summaryDreamTeller.assistant}`
   resultTotal.appendChild(astrologerMessage)
 }
 
