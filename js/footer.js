f336ffe45e7e4cfc977e624f0b32d707bab7ff0d
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-05-12 00:09:20 +0900

    v0.494 dream complete

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 const footer = document.querySelector(`.footer`)
 const headerForAd = document.querySelector(`.header`)
 
-const appVersion = '0.493'
+const appVersion = '0.494'
 
 function init() {
   footer.innerHTML = ` <div class="contact">
