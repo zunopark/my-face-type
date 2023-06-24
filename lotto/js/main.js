58a20fd62660fdf82cb44bc6f334dee0cd5f84ec
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-06-24 16:51:30 +0900

    t

--- lotto/js/main.js
+++
@@ -1,8 +0,0 @@
-const yangbanAppBtn = document.querySelector(`.yangban_app_btn`)
-
-const handleAppBtn = () => {
-  location.href = 'http://pf.kakao.com/_Qfuvxj/chat'
-  gtag('event', '카카오 플러스 친구 로또')
-}
-
-yangbanAppBtn.addEventListener('click', handleAppBtn)
