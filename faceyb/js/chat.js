8f5522e748c78f50fe31db6d35c6085d0b05abf6
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-06-30 17:05:15 +0900

    t

--- faceyb/js/chat.js
+++ faceyb/js/chat.js
@@ -102,7 +102,7 @@ const chatInit = () => {
       <div class="container_yangban_chat">${data.assistant}</div>
       <div class="container_yangban_chat">
                 저와 대화는 어떠셨나요? 힘이 되셨나요🙂 양반들은 모두 무료로
-                운영되고 있어요. 더욱 좋은 행운을 담아드리기 위해 작은 정성으로 응원해주시면 진심으로 감사드립니다! 
+                운영되고 있어요. 더욱 좋은 행운을 담아드리기 위해 작은 정성으로 응원해주시면 진심으로 감사드립니다! [국민은행: 652702-04-352074]
 
                 <a
                   style="text-decoration: none; color: black; width: 100%;"
