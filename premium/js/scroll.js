2531773c26604da133b5be8900a06f3116b03d18
dydtjq94 <dydtjq94@yonsei.ac.kr>
2023-07-23 12:06:45 +0900

    t

--- premium/js/scroll.js
+++ premium/js/scroll.js
@@ -1,24 +1,24 @@
-const scroll = document.querySelectorAll(`.scroll`)
+// const scroll = document.querySelectorAll(`.scroll`)
 
-function scrollAppear(e) {
-  e.style.transition = 'all 1s ease-in-out'
-  e.classList.add('6')
-  window.addEventListener('scroll', () => {
-    let elPos = e.getBoundingClientRect().top,
-      pos = window.innerHeight / 1
-    if (elPos < pos) {
-      e.classList.add('appear')
-    } else {
-      e.classList.remove('appear')
-    }
-  })
-}
+// function scrollAppear(e) {
+//   e.style.transition = 'all 1s ease-in-out'
+//   e.classList.add('6')
+//   window.addEventListener('scroll', () => {
+//     let elPos = e.getBoundingClientRect().top,
+//       pos = window.innerHeight / 1
+//     if (elPos < pos) {
+//       e.classList.add('appear')
+//     } else {
+//       e.classList.remove('appear')
+//     }
+//   })
+// }
 
-const scrollInit = () => {
-  scroll.forEach((item) => {
-    scrollAppear(item)
-  })
-  window.scrollTo(1, 1)
-}
+// const scrollInit = () => {
+//   scroll.forEach((item) => {
+//     scrollAppear(item)
+//   })
+//   window.scrollTo(1, 1)
+// }
 
-scrollInit()
+// scrollInit()
