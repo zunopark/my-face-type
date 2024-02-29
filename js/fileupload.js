0896b6315b35c5bf275f03c60de0270d3b1a8a97
dydtjq94 <dydtjq94@yonsei.ac.kr>
2024-02-29 16:17:10 +0900

    t

--- js/fileupload.js
+++ js/fileupload.js
@@ -24,6 +24,31 @@ async function readURL(input) {
     const imageTitleWrap = document.querySelector(`.ai`)
     imageTitleWrap.classList.add('disblock')
     borderCont.classList.add('disblock')
+
+    // 모달을 가져옵니다
+    var modal = document.getElementById('myModal')
+
+    // 모달을 여는 버튼을 가져옵니다
+    var btn = document.getElementById('openModal')
+
+    // 모달을 닫는 <span> 요소를 가져옵니다
+    var span = document.getElementsByClassName('close')[0]
+
+    // 사용자가 버튼을 클릭하면 모달을 엽니다
+
+    modal.style.display = 'block'
+
+    // 사용자가 <span> (x)를 클릭하면 모달을 닫습니다
+    span.onclick = function () {
+      modal.style.display = 'none'
+    }
+
+    // 사용자가 모달 외부를 클릭하면 모달을 닫습니다
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none'
+      }
+    }
   } else {
     removeUpload()
   }
