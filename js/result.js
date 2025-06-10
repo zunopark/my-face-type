6cbf16b2fcee14f1c57b7b4d8f6e6b058697bd25
dydtjq94 <ghdtlr90@gmail.com>
2025-06-10 22:39:21 +0900

    B

--- js/result.js
+++ js/result.js
@@ -252,7 +252,7 @@ async function startTossPayment(resultId) {
 
   try {
     const paymentWidget = PaymentWidget(clientKey, customerKey);
-    const paymentMethodWidget = paymentWidget.renderPaymentMethods("#payment-method", { value: 2900 });
+    const paymentMethodWidget = paymentWidget.renderPaymentMethods("#payment-method", { value: 1900 });
     paymentWidget.renderAgreement("#agreement");
 
     document.getElementById("payment-button").onclick = async () => {
